0d74752843580cf533212453ba705d03174737ac
rishitha0809 <146108099+rishitha0809@users.noreply.github.com>
2025-07-31 19:09:11 +0530

    ..

--- bbms-home.php
+++ bbms-home.php
@@ -8,7 +8,7 @@ session_start();
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Admin Login</title>
-	<link rel="stylesheet" type="text/css" href="css/s1.css">
+	<link rel="stylesheet" type="text/css" href="s1.css">
 </head>
 <body>
 <div class="full">
